1a18b897131a64a0eaa72e3633c90c029f8dff78
czerkies <roman.czerkies@gmail.com>
2016-08-01 18:10:29 +0200

    Feat(template.php): Sémantique HTML pour test template
    Test des valeurs retournées par le controller sur le template.php
    
    Issue #8

--- views/template.php
+++ views/template.php
@@ -1,3 +1,11 @@
-<?php
-
-echo $buffer;
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title><?php if(isset($meta['title'])) echo $meta['title']; ?></title>
+    <meta name="description" content="<?php if(isset($meta['description'])) echo $meta['description']; ?>">
+  </head>
+  <body>
+    <?= $buffer; ?>
+  </body>
+</html>
