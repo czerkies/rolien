7249008f909d5e602624a8c564df05ac716ec20e
czerkies <roman.czerkies@gmail.com>
2016-08-01 08:21:24 +0200

    Fix(models): Vérification de la requete avant fetch.
    Vérification du bon fonctionnement de la requete avant de récupérer les donnnées.
    
    Issue #7

--- models/queryModel.php
+++ models/queryModel.php
@@ -1,13 +1,13 @@
 <?php
 
 /**
- * Titre
+ * Gestion des requêtes basiques.
  *
- * Description
+ * 4 Method CRUD basique. La classe est instancié avec pour argument le nom de la 'table'.
  *
- * @version Release: v1.0.0
+ * @version v11.0.0
  * @link http://romanczerkies.fr/
- * @since Class available since Release v1.0.0-alpha.1
+ * @since v11.0.0-alpha.1
  */
 class queryModel extends connectModel {
 
@@ -16,12 +16,18 @@ class queryModel extends connectModel {
   }
 
   /**
-  * Affichage Test
+  * Requête 'SELECT'
   *
-  * @param
+  * @param Array $conditions
+  * 'column' => Collonne à retourner.
+  * 'where' => Trie des valeurs à récupérer.
+  * 'orderby' => Classement des données.
+  * 'order' => Classment croissant ou décroissant. ('DESC', 'ASC')
+  * 'limit' => Nombre de requete à récupérer.
+  * 'format' => Type de récupération des donnéees PDO ('rows', 'oneRow', 'nbRows')
   * @return Object $pdo Instance de la connexion via PDO
   */
-  public function selectDB($conditions = NULL) {
+  public function read($conditions = NULL) {
 
     $sql = "SELECT ";
 
@@ -57,7 +63,7 @@ class queryModel extends connectModel {
         else $this->displayError(__CLASS__, __FUNCTION__, "'order' doit être égal à 'DESC' ou 'ASC'.");
 
       }
-      
+
     }
 
     // Vérification limit
@@ -72,13 +78,21 @@ class queryModel extends connectModel {
 
     $datas = $this->pdo()->query($sql);
 
-    // Format de récupération des données.
-    if(!isset($conditions['format']) || $conditions['format'] === 'rows') $datasFormat = $datas->fetchAll(PDO::FETCH_ASSOC);
-    elseif($conditions['format'] === 'oneRow') $datasFormat = $datas->fetch(PDO::FETCH_ASSOC);
-    elseif($conditions['format'] === 'nbRows') $datasFormat = $datas->rowCount();
-    else $this->displayError(__CLASS__, __FUNCTION__, "Le format demandé doit être égal à 'oneRow', 'nbRows' ou NULL.");
+    if($datas) {
 
-    return $datasFormat;
+      // Format de récupération des données.
+      if(!isset($conditions['format']) || $conditions['format'] === 'rows') $datasFormat = $datas->fetchAll(PDO::FETCH_ASSOC);
+      elseif($conditions['format'] === 'oneRow') $datasFormat = $datas->fetch(PDO::FETCH_ASSOC);
+      elseif($conditions['format'] === 'nbRows') $datasFormat = $datas->rowCount();
+      else $this->displayError(__CLASS__, __FUNCTION__, "Le format demandé doit être égal à 'oneRow', 'nbRows' ou NULL.");
+
+      return $datasFormat;
+
+    } else {
+
+      $this->displayError(__CLASS__, __FUNCTION__, 'Erreur dans votre requete.');
+
+    }
 
   }
 
