4da6d67233477caf2bfee97cd558b59e847bfea2
czerkies <roman.czerkies@gmail.com>
2016-09-11 11:34:18 +0200

    Feat(/models/): MAJ method 'metaDatas'
    Récupération des bonnes valeurs et affichage erreur.
    
    Issues #7

--- models/superModel.php
+++ models/superModel.php
@@ -43,21 +43,20 @@ class superModel extends superController {
 
   }
 
-  public function metaDatas($file_name = 'undefind') {
+  public function metaDatas($file_name) {
 
-    //$sqlVerif = "SELECT file_name FROM pages WHERE file_name = '$file_name'";
-    //$exist = $this->pdo()->query($sqlVerif);
+    if(is_string($file_name) && !empty($file_name)) {
 
-    //if(!$exist->rowCount()) $file_name = '400';
-    //if($exist->rowCount()) {
-
-      $sql = "SELECT file_name, folder, title, description, restriction, function FROM pages WHERE file_name = '$file_name'";
+      $sql = "SELECT file_name, folder, title, description, restriction FROM pages WHERE file_name = '$file_name'";
       $datas = $this->pdo()->query($sql);
 
       return $datasPage = $datas->fetch(PDO::FETCH_ASSOC);
 
-    //}
+    } else {
 
+      $this->displayError(__CLASS__, __FUNCTION__, "'file_name' non valide.");
+
+    }
 
   }
 
