8e58e5a6566304d3e2fdd0956b9bcb1854d99152
czerkies <roman.czerkies@gmail.com>
2016-10-18 08:22:24 +0200

    Feat(/views/): JS pour recherche
    Recherche front avec ID.
    
    Issues #8

--- views/content/home.php
+++ views/content/home.php
@@ -1,22 +1,55 @@
 Je suis la page d'accueil.
 
-<form action="" method="POST">
-  <input type="search" name="s" value="<?php if(isset($_POST['s'])) echo htmlentities($_POST['s']); ?>" autofocus>
+<form action="#" method="" onsubmit="return:false;">
+  <input type="search" name="s" onkeyup="search(this.value)" value="<?php if(isset($_POST['s'])) echo htmlentities($_POST['s']); ?>" autocomplete="off">
   <button type="button" name="button">search</button>
 </form>
-
+<hr>
 <?php
-
-echo "<pre>";
-
-var_dump($_GET);
-
-echo "<hr>";
 
 //if(isset($vids)) var_dump($vids);
 
 if(isset($vids)):
   foreach ($vids as $value): ?>
-    Video : <b><?= $value['title']; ?></b>.<hr>
+    <div class="search" id="<?= $value['title'] . ' ' . $value['description']; ?>">Video : <b><?= $value['title']; ?></b>.(<?= $value['date']; ?>)<hr></div>
   <?php endforeach;
 endif; ?>
+
+
+<script type="text/javascript">
+  function node(arg, display) {
+
+    for(var i=0; i < 3; i++) {
+      console.log(arg[i]);
+      arg[i].style.display=display;
+    }
+
+  }
+
+  function search(key) {
+
+    var hide = document.querySelectorAll('.search');
+
+    if(key != '') {
+
+      var show = document.querySelectorAll('.search[id*="' + key + '"]');
+      /*for(var i=0; i < 3; i++) {
+        console.log(hide[i]);
+        hide[i].style.display="none";
+      }
+      for(var i=0; i < 3; i++) {
+        console.log(show[i]);
+        show[i].style.display="block";
+      }*/
+
+      node(hide, 'none');
+      node(show, 'block');
+
+    } else {
+
+      node(hide, 'block');
+
+    }
+
+  }
+</script>
